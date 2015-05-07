4328d43dc32f707206af61e020f2c6a6b016aea7
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2015-05-07 15:11:16 +0100

    First attempt to make the home page look like @rossjm 's designs.

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -4,7 +4,7 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
@@ -13,13 +13,13 @@ var cbpAnimatedHeader = (function() {
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-default' ),
 		didScroll = false,
-		changeHeaderOn = 300;
+		changeHeaderOn = 10;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
 			if( !didScroll ) {
 				didScroll = true;
-				setTimeout( scrollPage, 250 );
+				setTimeout( scrollPage, 150 );
 			}
 		}, false );
 	}
